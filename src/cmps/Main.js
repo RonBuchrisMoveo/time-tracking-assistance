4225a9467f40dc690f5216d0a010c14f138b1ea7
Ron Buchris <ronb@moveo.co.il>
2022-02-08 10:33:26 +0200

    check

--- src/cmps/Main.js
+++ src/cmps/Main.js
@@ -12,11 +12,27 @@ const [user, setUser] = useState();
           }`);
           setUser(data.me)
     }, []);
+
+    const getBoards=()=>{
+    //     let query = 'query {boards (ids: 2253447852) {name owner{ id }  columns {   title   type }}}';
+    //     fetch ("https://api.monday.com/v2", {
+    //    method: 'post',
+    //    headers: {
+    //      'Content-Type': 'application/json',
+    //      'Authorization' : 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE0NDUxNTIwNiwidWlkIjoyNjUwNjUxMSwiaWFkIjoiMjAyMi0wMi0wNlQxMzoxODoxMS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTEwMjMyOCwicmduIjoidXNlMSJ9.hBfGTpbOc86DGGbGcmmMD9gIg1P90Y8gvpgIGseWn5E'
+    //     },
+    //     body: JSON.stringify({
+    //       'query' : query
+    //     })
+    //    }).then(res => res.json())
+    //       .then(res => console.log(JSON.stringify(res, null, 2)));
+    }
     
 if(!user) return <div></div>
   return (
   <div>
       {user.name}
+      <button onClick={getBoards}>boards</button>
       </div>
       )
 };
