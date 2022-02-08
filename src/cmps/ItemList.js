b83e314b0f604829c50a412a5c04052cee2e523d
Ron Buchris <ronb@moveo.co.il>
2022-02-08 14:09:23 +0200

    v1

--- src/cmps/ItemList.js
+++ src/cmps/ItemList.js
@@ -2,20 +2,33 @@ import React, { useState } from 'react';
 
 export const ItemList = ({item}) => {
   console.log('item', item);
-  const [monthCounter, setMonthCounter] = useState(0);
+  const values = item.column_values
+  let status;
+  let CostPerHour;
+  let counter = 0
+  let hours = 0
   const months = ['January','February','March','April','May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
   return (
   <div className='item-container'>
-    {item.column_values.map(value=>{
+    {values.map(value=>{
       if(months.includes(value.title)){
       if(value.text !==''){
-       const hoursPerMonth = JSON.parse(value.text)
-       setMonthCounter(monthCounter+1)
-       console.log('monthCounter', monthCounter);
+        const hoursPerMonth = JSON.parse(value.text)
+        counter++
+        hours+=hoursPerMonth
       }
     }
-    })
-    }
+    if(value.title==='Status') status=value.text
+    if(value.title==='Hourly Rate') CostPerHour=value.text
+  })
+}
+<div>name:  {item.name}</div>
+<div>status:  {status}</div>
+<div>months counter:  {counter}</div>
+<div>hrs:  {hours}</div>
+<div>avg cost per month:  {(hours/counter)*CostPerHour} ₪</div>
+<div>predicted annual cost:  {((hours/counter)*12)*CostPerHour} ₪</div>
+<div>~~~~~~~~~~~~~~~~~~~~~~~~~~~~</div>
 
   </div>
   )
