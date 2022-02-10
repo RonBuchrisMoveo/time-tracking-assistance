30a40c53bdb9cf0aa37a9db81c6d042fea06c6bb
Ron Buchris <ronb@moveo.co.il>
2022-02-10 10:33:16 +0200

    search bar

--- src/cmps/SearchBar.js
+++ src/cmps/SearchBar.js
@@ -1,14 +1,15 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useState} from 'react';
 import { Dropdown,Flex } from "monday-ui-react-core";
 import "monday-ui-react-core/dist/main.css"
 
 export const SearchBar = ({users}) => {
     const options = useMemo(() => users, [])
+
       return <Flex gap={Flex.gaps.MEDIUM}>
               <div style={{
             width: "350px"
           }}>
-                <Dropdown options={options} multi className="dropdown-stories-styles_with-chips" />
+                <Dropdown onOptionRemove={(e)=>{console.log('e', e)}} options={options} multi className="dropdown-stories-styles_with-chips" />
               </div>
           </Flex>
     }
